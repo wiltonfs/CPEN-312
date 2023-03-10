3e8fe215756676e1bc1831a568b13d3e1839dd23
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-03-10 14:54:37 -0800

    Created Lab 4

---
+++ Lab 4/Assembly Examples/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\mmwil\Desktop\CPEN-312\Lab 4\Assembly Examples\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\mmwil\Desktop\CPEN-312\Lab 4\Assembly Examples\Step16.asm"
+echo Crosside_Action Set_Hex_File C:\Users\mmwil\Desktop\CPEN-312\Lab 4\Assembly Examples\Step16.HEX
