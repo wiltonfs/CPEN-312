c486f53ad82506b169c793a2a5c9e89e528017c1
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-03-10 15:59:19 -0800

    Displays 6 digits of stu #

---
+++ Lab 4/Lab4.asm
@@ -0,0 +1,44 @@
+$MODDE0CV ; Special Function Registers declaration for CV-8052
+
+org 0000H ; After reset, the processor starts at location zero
+	ljmp main
+    
+; Look-up table for my student number
+; Least sig to most sig
+; 48059760
+T_StuNum:
+	DB 40H, 02H, 78H, 10H, 12H, 40H, 00H, 19H
+T_HexDisp:
+	DB HEX0, HEX1, HEX2, HEX3, HEX4, HEX5
+
+Display_on mac
+	mov b, a ; Preserve value of accumulator
+	
+	mov dptr, #T_StuNum ; point to student number lookup table
+	mov a, %1 ; Load input into accumulator
+	movc a, @dptr+a ; Read from table
+	
+	mov %0, a ; Display number
+	mov a, b ; Restore value of accumulator
+endmac
+
+main:
+	mov SP, #0x7f
+	mov LEDRA, #0 ; Bit addressable
+	mov LEDRB, #0 ; Not bit addressable
+Forever:
+	mov r1, #00H
+	Display_on(HEX0,r1)
+	inc r1 ; increment r1
+	Display_on(HEX1, r1)
+	inc r1 ; increment r1
+	Display_on(HEX2, r1)
+	inc r1 ; increment r1
+	Display_on(HEX3, r1)
+	inc r1 ; increment r1
+	Display_on(HEX4, r1)
+	inc r1 ; increment r1
+	Display_on(HEX5, r1)
+
+	ljmp Forever ; Repeat forever
+END
--- Lab 4/__LOAD.bat
+++ Lab 4/__LOAD.bat
@@ -1,3 +1,3 @@
 @echo off
 ::This file was created automatically by CrossIDE to load a hex file using Quartus_stp.
-"C:\intelFPGA_lite\18.1\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\Users\mmwil\Desktop\CPEN-312\Lab 4\blinky.hex" | find /v "Warning (113007)"
+"C:\intelFPGA_lite\18.1\quartus\bin64\quartus_stp.exe" -t "C:\CrossIDE\Load_Script.tcl" "C:\Users\mmwil\Desktop\CPEN-312\Lab 4\Lab4.HEX" | find /v "Warning (113007)"
